dcc4805df3ba4204a6e1e69b29164e7539d510b1
shynur <shynur@outlook.com>
2026-01-20 16:15:03 +0800

    ;

--- src/proto.ts
+++ src/proto.ts
@@ -6,40 +6,40 @@ type Direction = [number, number]
 // Server-side 手动归一化.
 
 type BubbleBody = {
-  Radius: number,
-  Ate: string[],  // 吃了的气泡的 ID, 每吃一个在后面追加一个.
-  EatenBy?: string,  // 表示被谁吃了.
+	Radius: number,
+	Ate: string[],  // 吃了的气泡的 ID, 每吃一个在后面追加一个.
+	EatenBy?: string,  // 表示被谁吃了.
 }
 
 type ClientMessageRaw = {
-  User: {
-    ID: string,  // 客户端随机生成
-    Direction: Direction,
-    Skin?: any,  // 客户端内部使用
-  }
+	User: {
+		ID: string,  // 客户端随机生成
+		Direction: Direction,
+		Skin?: any,  // 客户端内部使用
+	}
 }
 
-type ClientMessage = {
-  Timestamp: number,  // UNIX 时间戳, 毫秒.
+export type ClientMessage = {
+	Timestamp: number,  // UNIX 时间戳, 毫秒.
 } & ClientMessageRaw
 
 type Kinematic = {
-  Coordinate: Coordinate,
-  Direction: Direction,
-  Speed: number,
+	Coordinate: Coordinate,
+	Direction: Direction,
+	Speed: number,
 }
 
 type Bubble = {
-  ID: string,
-  Body: BubbleBody,
-  Kinematic: Kinematic,
+	ID: string,
+	Body: BubbleBody,
+	Kinematic: Kinematic,
 }
 
 type ServerMessageRaw = {
-  Bubbles: Record<string, Bubble & {Skin?: any}>,
-  Players: string[],
+	Bubbles: Record<string, Bubble & {Skin?: any}>,
+	Players: string[],
 }
 
-type ServerMessage = {
-  Timestamp: number
+export type ServerMessage = {
+	Timestamp: number
 } & ServerMessageRaw
