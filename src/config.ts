f03e64b4a7f0130242507921c3a8c80f7e606e5e
shynur <shynur@outlook.com>
2026-01-21 16:46:28 +0800

    ;

--- src/config.ts
+++ src/config.ts
@@ -3,8 +3,11 @@ export const WEBSOCKET_PORT = 6666
 export const GAME_TICKS = 10
 export const GAME_TICK_PERIOD_MS = 1000 / GAME_TICKS
 
-export const [MAP_WIDTH, MAP_HEIGHT] = [500, 500]
+export const [MAP_WIDTH, MAP_HEIGHT] = function(x: number, y: number) {
+    return [Math.max(x, y), Math.min(x, y)]
+}(500, 500)
 export const [X_MAX, Y_MAX] = [MAP_WIDTH, MAP_HEIGHT].map(v => v / 2)
+export const MAX_BUBBLE_RADIUS = MAP_HEIGHT
 
 export const PLAYER_INIT_RADIUS = 10
 
@@ -14,3 +17,11 @@ export const INIT_NUM_NPC_BUBBLES = Math.floor(
 
 export const STOMACH_RADIUS_RATIO = 0.6
 // 另一个气泡的圆心进入到当前气泡圆心的这个比例位置时, 视为被吃掉.
+
+// 半径越大, 速度越慢.
+export function GetSpeedByRadius(radius: number): number {
+    const [MIN_SPEED, MAX_SPEED] = [5, 50]
+
+    const speed = MAX_SPEED - radius/MAX_BUBBLE_RADIUS * (MAX_SPEED - MIN_SPEED)
+    return speed
+}
--- src/game.ts
+++ src/game.ts
@@ -8,6 +8,8 @@ import {
     MAP_HEIGHT, MAP_WIDTH,
     GAME_TICK_PERIOD_MS,
     STOMACH_RADIUS_RATIO,
+    MAX_BUBBLE_RADIUS,
+    GetSpeedByRadius,
     PLAYER_INIT_RADIUS,
 } from './config.js'
 
@@ -30,9 +32,15 @@ class Bubble {
         other.Speed = 0
 
         other.EatenBy = this.ID
-        this.Ate.push(other.ID)
+        if (other.ID != '')  // trial bubbles have NULL-string ID
+            this.Ate.push(other.ID)
 
-        this.Radius = Math.sqrt(this.Radius**2 + other.Radius**2)
+        this.Radius = Math.min(
+            MAX_BUBBLE_RADIUS,
+            Math.sqrt(this.Radius**2 + other.Radius**2)
+        )
+        if (this.Speed)
+            this.Speed = GetSpeedByRadius(this.Radius)
     }
 
     Dist(other: typeof this): number {
@@ -69,9 +77,11 @@ class Bubble {
     }
 
     Move(delta_sec: number) {
-        this.NormalizeDirection()
-
+        if (this.Speed == 0)
+            return
         const dist = this.Speed * delta_sec
+
+        this.NormalizeDirection()
         this.Coordinate[0] += this.Direction[0] * dist
         this.Coordinate[1] += this.Direction[1] * dist
     }
@@ -127,25 +137,99 @@ export class Game {
         }
 
         this.Updater = status_reporter
+    }
+
+    Start() {
         setInterval(
             () => this.Execute(),
             GAME_TICK_PERIOD_MS
         )
     }
+
     Execute = (() => {
-        let last_cmd_timestamp = Date.now()
-        let last_cmd: ClientMessageRaw|null = null
+        let last_timestamp = Date.now()
 
         return (cmd?: ClientMessageRaw) => {
             const current_timestamp = Date.now()
+            const delta_sec = (current_timestamp - last_timestamp) / 1000
+            last_timestamp = current_timestamp
 
-            if (last_cmd === null) {
-                last_cmd = cmd
-                last_cmd_timestamp = current_timestamp
+            this.MoveAllBubbles(delta_sec)
+            const dead_bubbles = this.LetBubblesEatEachOther()
+
+            if (cmd == undefined) {
+                if (dead_bubbles.size) {
+                    this.Updater({
+                        Bubbles: Object.fromEntries([...this.Bubbles].map(([id, bubble]) => [id, bubble.Dump()])),
+                        Players: Array.from(this.Players),
+                    })
+                }
                 return
             }
 
-
+            const {
+                ID: user_id,
+                Direction: user_direction,
+            } = cmd.User
+            if (!this.Players.has(user_id)) {
+                this.AddUser(user_id)
+            }
+            const user_bubble = this.Bubbles.get(user_id)
+            if (String(user_direction) != '0,0') {
+                user_bubble.Direction = user_direction
+                user_bubble.Speed = GetSpeedByRadius(user_bubble.Radius)
+            }
+            this.Updater({
+                Bubbles: Object.fromEntries([...this.Bubbles].map(([id, bubble]) => [id, bubble.Dump()])),
+                Players: Array.from(this.Players),
+            })
         }
     })()
+
+    AddUser(user_id: string) {
+        let user_bubble_coordinate: [number, number]
+
+        while (true) {
+            const [x, y] = [(Math.random() - 0.5) * MAP_WIDTH, (Math.random() - 0.5) * MAP_HEIGHT]
+            let trial_bubble = new Bubble('', PLAYER_INIT_RADIUS, [x, y])
+
+            let is_candidate = true
+            for (const eater of this.Bubbles.values()) {
+                if (eater.TryEat(trial_bubble)) {
+                    is_candidate = false
+                    break
+                }
+            }
+
+            if (is_candidate) {
+                user_bubble_coordinate = [x, y]
+                break
+            }
+        }
+
+        const user_bubble = new Bubble(user_id, PLAYER_INIT_RADIUS, user_bubble_coordinate)
+        this.Bubbles.set(user_id, user_bubble)
+        this.Players.add(user_id)
+        console.log(`Added new player: ID=${user_id}`)
+    }
+
+    MoveAllBubbles(delta_sec: number) {
+        for (const bubble of this.Bubbles.values()) {
+            bubble.Move(delta_sec)
+        }
+    }
+
+    LetBubblesEatEachOther(): ReadonlySet<string> {
+        const dead_bubbles = new Set<string>
+
+        for (const eater of this.Bubbles.values()) {
+            for (const eaten of this.Bubbles.values()) {
+                if (eater.TryEat(eaten)) {
+                    dead_bubbles.add(eaten.ID)
+                }
+            }
+        }
+
+        return dead_bubbles
+    }
 }
