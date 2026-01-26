e9324cabd180e1980e9b172b3599c2caff8f574d
shynur <shynur@outlook.com>
2026-01-26 10:53:36 +0800

    ;

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import {WebSocketServer, type WebSocket} from 'ws'
+import Ajv from "ajv"
 import {
     WEBSOCKET_PORT,
 } from './config.js'
@@ -8,6 +9,30 @@ import type {
     ServerMessageRaw,
 } from './proto.js'
 import {Game} from './game.js'
+
+const client_json_validator = new Ajv().compile(
+    {
+        type: 'object',
+        properties: {
+            User: {
+                type: 'object',
+                properties: {
+                    ID: {type: 'string'},
+                    Direction: {
+                        type: 'array',
+                        items: {type: 'number'},
+                        minItems: 2,
+                        maxItems: 2,
+                    },
+                    Skin: {},
+                },
+                required: ['ID', 'Direction'],
+            },
+            Timestamp: {type: 'number'},
+        },
+        required: ['User', 'Timestamp'],
+    }
+)
 
 const sockets = new Set<WebSocket>()
 function Broadcast(msg: ServerMessageRaw) {
@@ -39,7 +64,18 @@ new WebSocketServer({
         }
 
         ws.onmessage = event => {
-            const msg: ClientMessage = JSON.parse(event.data.toString())
+            let msg_: ClientMessage
+            try {
+                msg_ = JSON.parse(event.data.toString())
+            } catch {
+                console.warn('Received non-JSON message from client')
+                return
+            }
+            const msg = msg_
+            if (!client_json_validator(msg)) {
+                console.warn('Received invalid message from client:', client_json_validator.errors)
+                return
+            }
             console.log('Received message from client:', msg)
             game.Execute(msg)
         }
